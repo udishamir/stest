3bf1e97fa7dd6512d943ed3405b5e12c1857f2ad
udi <udishamir@gmail.com>
2017-08-04 08:38:43 -0700

    Adding more

--- conhttp.c
+++ conhttp.c
@@ -1,3 +1,16 @@
+/*
+ *  ___________      ___________
+ *  \__    ___/____ /_   \   _  \   ____
+ *    |    |  \__  \ |   /  /_\  \ /    \
+ *    |    |   / __ \|   \  \_/   \   |  \
+ *    |____|  (____  /___|\_____  /___|  /
+ *                       \/           \/     \/
+ *  This function utilize WinHTTP library \
+ *  it will test IOC performance
+ * 
+ * 
+*/
+
 #include <windows.h>
 #include <winhttp.h>
 #include <stdio.h>
@@ -13,21 +26,25 @@ int main(){
 
 
     // Use WinHttpOpen to obtain a session handle.
-    hSession = WinHttpOpen(L"Testing HTTP Socket/1.0",
+    hSession = WinHttpOpen(L"IOC HTTP Socket/1.0 Test Suite",
 		      WINHTTP_ACCESS_TYPE_DEFAULT_PROXY,
 		      WINHTTP_NO_PROXY_NAME,
 		      WINHTTP_NO_PROXY_BYPASS, 0);
     if(hSession){
+      // we have valid session
       hConnect = WinHttpConnect(hSession, L"www.microsoft.com",
 		      INTERNET_DEFAULT_HTTPS_PORT, 0);
     }
 
     if(hConnect){
-      // we have connection handle
-      WinHttpOpenRequest( hConnect, L"GET", NULL,
+      // we have valid connection handle
+      hRequest = WinHttpOpenRequest( hConnect, L"GET", NULL,
+		      INTERNET_DEFAULT_HTTPS_PORT, 0 ); // will be changed
 
 
-
+    // creating http request 
+     
+    
     // we are done here
     goto cleanup;
 
