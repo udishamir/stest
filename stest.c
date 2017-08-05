d6f309054697eb67866180562f61699766f2aec5
udi <udishamir@gmail.com>
2017-08-04 17:46:30 -0700

    Adding more relevant setup before calling HTTP

--- stest.c
+++ stest.c
@@ -1,19 +1,63 @@
 #include <windows.h>
 #include <stdio.h>
 
+#include "config.h"
+
+// prototypes 
 int FilesCreate(UINT NumberOffiles);
 int ConnectWinHttp();
 
-int main(int argc, char *argv[]){
-   // calling FileCreate for random file creation
+__int64 GetSitesListSize(){
+    HANDLE hFile;
+    BOOL   flStat;
+    LARGE_INTEGER szFilesize;
    
-   UINT NumberOffiles = 5;
+    // open sites.txt for reading
+    hFile = CreateFile(siteurl, 
+		    0,
+		    0,
+		    NULL,
+		    OPEN_EXISTING,
+		    FILE_ATTRIBUTE_NORMAL,
+		    NULL);
 
-   FilesCreate(NumberOffiles);
+    if(hFile == INVALID_HANDLE_VALUE){
+	    printf("failed to open url file error: %d\n", GetLastError());
+	    return 0;
+    }
 
-   // testing WinHTTP IOC
-   ConnectWinHttp("www.microsoft.com");
+    flStat = GetFileSizeEx(hFile, &szFilesize);
+    if(FALSE == flStat){
+	    // closing handle
+	    CloseHandle(hFile);
 
-   return 0;
+	    printf("retrieveing file size failed with error: %d\n", GetLastError());
+	    return 0;
+    }
+
+    CloseHandle(hFile);
+
+    // return signed 
+    return szFilesize.QuadPart;
 }
 
+int main(int argc, char *argv[]){
+    UINT NumberOffiles = 5;
+    int szTestFile;
+
+    szTestFile = GetSitesListSize();
+    if(szTestFile){
+	    printf("url file size %db\n", szTestFile);
+
+    	    // calling FileCreate for random file creation
+    	    FilesCreate(NumberOffiles);
+
+    	    // testing WinHTTP IOC
+    	    ConnectWinHttp("www.microsoft.com");
+    }else{
+	    printf("error reading site url file, going out ...\n");
+	    return -1;
+    }
+
+    return 0;
+}
